e61eb401b2a0bfdb3eebf3efb66fd9bf9718ef31
joshmeranda <joshua.meranda@gmail.com>
2024-06-09 14:23:57 -0400

    integrate impersonation in to terminal service

--- gateway/terminal.go
+++ gateway/terminal.go
@@ -6,6 +6,7 @@ import (
 
 	marinav1 "github.com/joshmeranda/marina-operator/api/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/joshmeranda/marina/apis/terminal"
@@ -14,6 +15,11 @@ import (
 var _ terminal.TerminalServiceServer = &Gateway{}
 
 func (g *Gateway) CreateTerminal(ctx context.Context, req *terminal.TerminalCreateRequest) (*empty.Empty, error) {
+	kubeClient, err := g.clientFromContext(ctx, client.Options{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to impersonate user: %w", err)
+	}
+
 	terminal := marinav1.Terminal{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      req.Name.Name,
@@ -22,7 +28,7 @@ func (g *Gateway) CreateTerminal(ctx context.Context, req *terminal.TerminalCrea
 		Spec: marinav1.TerminalSpec{},
 	}
 
-	if err := g.kubeClient.Create(ctx, &terminal); err != nil {
+	if err := kubeClient.Create(ctx, &terminal); err != nil {
 		return nil, err
 	}
 
@@ -30,6 +36,11 @@ func (g *Gateway) CreateTerminal(ctx context.Context, req *terminal.TerminalCrea
 }
 
 func (g *Gateway) DeleteTerminal(ctx context.Context, req *terminal.TerminalDeleteRequest) (*empty.Empty, error) {
+	kubeClient, err := g.clientFromContext(ctx, client.Options{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to impersonate user: %w", err)
+	}
+
 	terminal := marinav1.Terminal{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      req.Name.Name,
@@ -37,7 +48,7 @@ func (g *Gateway) DeleteTerminal(ctx context.Context, req *terminal.TerminalDele
 		},
 	}
 
-	if err := g.kubeClient.Delete(ctx, &terminal); err != nil {
+	if err := kubeClient.Delete(ctx, &terminal); err != nil {
 		return nil, fmt.Errorf("could not delete terminal '%s': %w", req.Name, err)
 	}
 
